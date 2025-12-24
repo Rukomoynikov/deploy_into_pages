977332636e7964e457eaa10cbd09ab76f9692e03
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-12-24 16:31:02 +0000

    Remove unnecessary files

--- src/create_page_deployment.ts
+++
@@ -1,3 +0,0 @@
-const createPageDeployment = () => {};
-
-export { createPageDeployment };
--- test/create_page_deployment.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { createPageDeployment } from "./src/create_page_deployment.ts";
-
-test("it sends request to github api", () => {
-  createPageDeployment();
-});
