6a83824baf7040428413099ebc73287d7c0aba8c
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-12-25 13:49:47 +0000

    Enhance context module to include repository owner and update token handling

--- src/context.ts
+++ src/context.ts
@@ -1,30 +1,32 @@
 const context = {
   workflowRunID: process.env.GITHUB_RUN_ID,
   repositoryNwo: process.env.GITHUB_REPOSITORY,
+  repositoryOwner: process.env.GITHUB_REPOSITORY_OWNER,
   commitSHA: process.env.GITHUB_SHA,
   actionsId: process.env.GITHUB_ACTION,
-  githubToken: process.env.GITHUB_TOKEN,
-  githubApiUrl: process.env.GITHUB_API_URL ?? 'https://api.github.com',
-  githubServerUrl: process.env.GITHUB_SERVER_URL ?? 'https://github.com',
-  artifactName: process.env.ARTIFACT_NAME,
-}
+  githubToken: process.env.INPUT_TOKEN,
+  githubApiUrl: process.env.GITHUB_API_URL ?? "https://api.github.com",
+  githubServerUrl: process.env.GITHUB_SERVER_URL ?? "https://github.com",
+  artifactName: process.env.INPUT_ARTIFACT_NAME,
+};
 
 const withCheckedValues = (ctx: typeof context) => {
   for (const [key, value] of Object.entries(ctx)) {
-    if (value === undefined || value === '') {
-      throw new Error(`Missing required context value: ${key}`)
+    if (value === undefined || value === "") {
+      throw new Error(`Missing required context value: ${key}`);
     }
   }
   return ctx as {
-    workflowRunID: string
-    repositoryNwo: string
-    commitSHA: string
-    actionsId: string
-    githubToken: string
-    githubApiUrl: string
-    githubServerUrl: string
-    artifactName: string
-  }
-}
+    workflowRunID: string;
+    repositoryNwo: string;
+    repositoryOwner: string;
+    commitSHA: string;
+    actionsId: string;
+    githubToken: string;
+    githubApiUrl: string;
+    githubServerUrl: string;
+    artifactName: string;
+  };
+};
 
-export { context, withCheckedValues }
+export { context, withCheckedValues };
